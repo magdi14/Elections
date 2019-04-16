ed25d0207675428b2178f92425dcf26e2bf841eb
mohamed magdy <mohamed14magdi@gmail.com>
2019-04-16 20:39:15 +0200

    fixed the permutation

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "mpi.h"
 
 void changeValues (int *a, int *b)
 {
@@ -8,20 +9,24 @@ void changeValues (int *a, int *b)
     *a = *b;
     *b = temp;
 }
-void shuffleRandon ( int arr1[], int n )
+void shuffleRandon ( int array[], int n )
 {
-    srand ( time(0) );
-    for (int i = n-1; i > 0; i--)
+    if (n > 1)
     {
-        int j = rand() % (i+1);
-        changeValues(&arr1[i], &arr1[j]);
+        size_t i;
+        for (i = 0; i < n - 1; i++)
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
     }
 }
 void CreateInputFile(int cand, int voters)
 {
     FILE *filePointer;
-    int i, t, j;
-    int min = 1, max = cand;
+    int i, j;
     filePointer = fopen("Voters.txt", "w");
     if ( filePointer == NULL )
     {
@@ -29,14 +34,15 @@ void CreateInputFile(int cand, int voters)
     }
     else
     {
-        printf("The file is now opened.\n");
+        //printf("The file is now opened.\n");
         fprintf(filePointer, "%d \n", cand);
         fprintf(filePointer, "%d \n", voters);
         int arr[cand];
         for (i=0; i<cand; i++)
             arr[i] = i+1;
-        for (i=0; i<cand; i++)
+        /*for (i=0; i<cand; i++)
             printf("%d ", arr[i]);
+        */
 
         for (i=0; i<voters; i++)
         {
@@ -50,8 +56,40 @@ void CreateInputFile(int cand, int voters)
         fclose(filePointer);
     }
 }
-int main()
+int main(int argc , char * argv[])
 {
-    CreateInputFile(5,8);
+
+	int my_rank;		/* rank of process	*/
+	int p;			/* number of process	*/
+	int source;		/* rank of sender	*/
+	int dest;		/* rank of reciever	*/
+	int tag = 0;
+	int cand, voters;
+    		/* tag for messages	*/
+		/* storage for message	*/
+	MPI_Status status;	/* return status for 	*/
+				/* recieve		*/
+
+	/* Start up MPI */
+	MPI_Init( &argc , &argv );
+
+	/* Find out process rank */
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	/* Find out number of process */
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+    if(my_rank==0)
+    {
+        printf("How many candidates ?\n");
+        scanf("%d", &cand);
+        printf("Enter the number of Voters:\n");
+        scanf("%d", &voters);
+        CreateInputFile(cand,voters);
+    }
+
+
+
+    MPI_Finalize();
     return 0;
 }
